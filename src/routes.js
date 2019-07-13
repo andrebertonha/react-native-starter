cd774e75365426cd9a7f45767222476a25bb5546
André Martins <andrebertonhadev@gmail.com>
2019-07-13 14:53:46 -0300

    react native starter project

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+
+import Main from './pages/main';
+import Product from './pages/product';
+
+const MainNavigator =  createStackNavigator({
+    Main,
+    Product
+});
+
+const App = createAppContainer(MainNavigator);
+
+export default App;
